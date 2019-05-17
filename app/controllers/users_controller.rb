4383dcf30c5f722c1ce0a3506f690fa413122b0c
Bart Karnath <karnath.bart@gmail.com>
2019-05-17 17:37:24 +0100

    user controller, routes and show and dashboard views added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def dashboard
+    @user = current_user
+    authorize @user
+    @cameras = Camera.where(user: current_user)
+  end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :cameras, only: [:index, :show, :new, :create, :destroy]
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :users, only: :show
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/ro
+  get 'dashboard', to: 'users#dashboard', as: :dashboard
 end
