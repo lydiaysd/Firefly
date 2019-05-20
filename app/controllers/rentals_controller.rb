c01c21e88ac381bee9fef1db49e4374c18ffb77a
Bart Karnath <karnath.bart@gmail.com>
2019-05-20 19:55:09 +0100

    rental controller, model and views updated

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,5 +1,17 @@
 class RentalsController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :find_camera, only: [:show, :new, :create]
+
+  def index
+    @user = current_user
+    @rentals = Rental.where(user: @user)
+    @cameras = Camera.where(user: @user)
+  end
+
+  def show
+    @user = current_user
+    @rental = Rental.find(param[:id])
+  end
 
   def new
     @rental = Rental.new
@@ -7,12 +19,22 @@ class RentalsController < ApplicationController
   end
 
   def create
-    @rental = Rental.new(rental_params)
+    @rental = Rental.new(user: current_user)
     authorize @rental
+    @rental.camera = @camera
+    if @rental.save
+      redirect_to camera_rentals_path
+    else
+      render :new
+    end
   end
 
   private
 
-  def rental_params
+  def find_camera
+    @camera = Camera.find(params[:camera_id])
   end
+  # def rental_params
+  #   params.require(:rental).permit(:user_id, :camera_id)
+  # end
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,6 +1,6 @@
 class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :camera
-  validates :start_date, presence: true
-  validates :end_date, presence: true
+  # validates :start_date, presence: true
+  # validates :end_date, presence: true
 end
