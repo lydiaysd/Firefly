d818afa8a22066c7905eddc81c35d753ca156e50
Ana Ghidelli <ana.ghidelli@gmail.com>
2019-05-22 14:20:59 +0100

    fixed navbar and login pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,26 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :store_user_location!, if: :storable_location?
+
   include Pundit
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    # :user is the scope we are authenticating
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
+  end
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
